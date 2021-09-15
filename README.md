33f2396d259eba1412eb336f5f8bd9736db9e527
quartie-gif <62614346+quartie-gif@users.noreply.github.com>
2021-09-15 22:29:17 +0200

    Qt dev

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OOP_Practice
 
+## Table of contents
+* [General info](#general-info)
+
+
+## General info
+In this git repo I update work that I've been doing studying c++ language.
--- Random_things/Templates/main.cpp
+++ Random_things/Templates/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream> 
 #include "Templates.h"
+#include "tempTemp.h"
 
 int main()
 {
@@ -13,6 +14,28 @@ int main()
 	Ob1.print();
 	std::cout << Ob1.getFirst() << std::endl;
 
+	std::cout << "----------------------------- tempTemp ------------------------\n";
+
+	using Type = std::pair<Float, Float>;
+
+	A<std::pair<Float, Float>> obj(std::pair<Float, Float> (3.3, 4.3)) ;
+
+	std::cout << "First: " << obj->_val1.first << "\tSecond: " << obj->_val1.second << std::endl;
+
+	A<Type>* ptrA = new A<Type>(Type(1.3, 3.3));
+
+	std::cout << *ptrA << std::endl;
+
+	obj = obj + *ptrA;
+
+	obj.print(std::cout);
+
+	X<float, std::pair> other(obj._val1);
+
+
+
+
+	delete ptrA;
 
 	return 0;
 }
